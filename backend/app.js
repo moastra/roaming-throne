df6a12f5f698a96b0d1203c1562e1416a46bd303
Mathew M <mathewmastromonaco@gmail.com>
2024-09-24 14:07:08 -0400

    backend w/ database setup and frontend setup with CRA

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+const db = require('./db');
+const dbHelpers = require('./db/helpers/dbHelpers')(db);
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/users', usersRouter(dbHelpers));
+
+module.exports = app;
